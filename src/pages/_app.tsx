1752e5420f9b8209554f3924c1c71762b520b7c8
Walefe de Jesus <walefedev@gmail.com>
2022-05-03 19:55:02 -0300

    feat: config page app with providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { theme } from '../styles/themes'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
